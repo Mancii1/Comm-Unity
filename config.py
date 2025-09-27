529ceaf945a1e4d1fcd159fb1c26b3e02e418518
Lungelo Manci <104348379+Mancii1@users.noreply.github.com>
2025-09-27 09:40:24 +0200

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ load_dotenv()
 app = Flask(__name__)
 CORS(app)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("SUPABASE_DB_URI")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
